b5aae180c2e9806ac05ce1d8916b2c04487d9d99
franck <carnet.franck.paul@gmail.com>
2023-03-01 12:06:03 +0100

    Use formXXX::init()

--- src/Manage.php
+++ src/Manage.php
@@ -18,7 +18,15 @@ use dcCore;
 use dcNsProcess;
 use dcPage;
 use Exception;
-use form;
+use formCheckbox;
+use formFieldset;
+use formForm;
+use formInput;
+use formLabel;
+use formLegend;
+use formPara;
+use formSubmit;
+use formText;
 use html;
 use http;
 
@@ -102,6 +110,37 @@ class Manage extends dcNsProcess
     }
 
     /**
+     * Opens a module.
+     *
+     * Will be replaced by dcPage::openModule() in 2.26
+     *
+     * @param      string       $title  The title
+     * @param      null|string  $head   The head
+     */
+    public static function openModule(string $title = DC_VENDOR_NAME, ?string $head = '')
+    {
+        echo
+        '<html>' .
+        '<head>' .
+        '<title>' . $title . '</title>' .
+        $head .
+        '</head>' .
+        '<body>';
+    }
+
+    /**
+     * Closes a module.
+     *
+     * Will be replaced by dcPage::closeModule() in 2.26
+     */
+    public static function closeModule()
+    {
+        echo
+        '</body>' .
+        '</html>';
+    }
+
+    /**
      * Renders the page.
      */
     public static function render(): void
@@ -121,12 +160,7 @@ class Manage extends dcNsProcess
         $sm_description     = dcCore::app()->blog->settings->socialMeta->description;
         $sm_image           = dcCore::app()->blog->settings->socialMeta->image;
 
-        echo
-        '<html>' .
-        '<head>' .
-        '<title>' . __('socialMeta') . '</title>' .
-        '</head>' .
-        '<body>';
+        self::openModule(__('socialMeta'));
 
         echo dcPage::breadcrumb(
             [
@@ -136,82 +170,193 @@ class Manage extends dcNsProcess
         );
         echo dcPage::notices();
 
-        echo
-        '<form action="' . dcCore::app()->admin->getPageURL() . '" method="post">' .
-        '<p>' . form::checkbox('sm_active', 1, $sm_active) . ' ' .
-        '<label for="sm_active" class="classic">' . __('Active socialMeta') . '</label></p>' .
+        echo formForm::init('frmreport')
+            ->action(dcCore::app()->admin->getPageURL())
+            ->method('post')
+            ->fields([
+                // Activation
+                formPara::init()
+                    ->separator(' ')
+                    ->items([
+                        formCheckbox::init('sm_active', $sm_active)
+                            ->value(1),
+                        formLabel::init(__('Active socialMeta'))
+                            ->for('sm_active')
+                            ->class('classic'),
+                    ]),
+                // Options
+                formFieldset::init('sm_opt')
+                    ->legend(formLegend::init(__('Options')))
+                    ->class('fieldset')
+                    ->fields([
+                        (formPara::init())
+                            ->separator(' ')
+                            ->items([
+                                formCheckbox::init('sm_on_post', $sm_on_post)
+                                    ->value(1),
+                                formLabel::init(__('Add social meta on posts'))
+                                    ->for('sm_on_post')
+                                    ->class('classic'),
+                            ]),
+                        formPara::init()
+                            ->separator(' ')
+                            ->items([
+                                formCheckbox::init('sm_on_page', $sm_on_page)
+                                    ->value(1),
+                                formLabel::init(__('Add social meta on pages'))
+                                    ->for('sm_on_page')
+                                    ->class('classic'),
+                            ]),
+                        formText::init('hr'),
+                        // Specific Facebook
+                        formPara::init()
+                            ->separator(' ')
+                            ->items([
+                                formCheckbox::init('sm_facebook', $sm_facebook)
+                                    ->value(1),
+                                formLabel::init(__('Use Open Graph (Mastodon/Facebook) social meta:'))
+                                    ->for('sm_facebook')
+                                    ->class('classic'),
+                            ]),
+                        formText::init(
+                            'pre',
+                            html::escapeHTML(
+                                '<!-- Open Graph (Mastodon/Facebook) -->' . "\n" .
+                                '<meta property="og:title" content="Plugin socialMeta 0.2 pour Dotclear" />' . "\n" .
+                                '<meta property="og:url" content="http://open-time.net/post/2014/01/20/Plugin-socialMeta-02-pour-Dotclear" />' . "\n" .
+                                '<meta property="og:site_name" content="Open-Time" />' . "\n" .
+                                '<meta property="og:description" content="Nouvelle version de ce petit plugin, ..." />' . "\n" .
+                                '<meta property="og:image" content="http://open-time.net/public/illustrations/2014/.googleplus-twitter-facebook_m.jpg" />' . "\n" .
+                                '<meta property="og:image:alt" content="G+, Twitter et Facebook"/>' . "\n"
+                            )
+                        ),
+                        // Specific Google
+                        formPara::init()
+                            ->separator(' ')
+                            ->items([
+                                formCheckbox::init('sm_google', $sm_google)
+                                    ->value(1),
+                                formLabel::init(__('Use Google social meta:'))
+                                    ->for('sm_google')
+                                    ->class('classic'),
+                            ]),
+                        formText::init(
+                            'pre',
+                            html::escapeHTML(
+                                '<!-- Google -->' . "\n" .
+                                '<meta itemprop="name" content="Plugin socialMeta 0.2 pour Dotclear" />' . "\n" .
+                                '<meta itemprop="description" content="Nouvelle version de ce petit plugin, ..." />' . "\n" .
+                                '<meta itemprop="image" content="http://open-time.net/public/illustrations/2014/.googleplus-twitter-facebook_m.jpg" />' . "\n"
+                            )
+                        ),
+                        // Specific Twitter
+                        formPara::init()
+                            ->separator(' ')
+                            ->items([
+                                formCheckbox::init('sm_twitter', $sm_twitter)
+                                    ->value(1),
+                                formLabel::init(__('Use Twitter social meta:'))
+                                    ->for('sm_twitter')
+                                    ->class('classic'),
+                            ]),
+                        formText::init(
+                            'pre',
+                            html::escapeHTML(
+                                '<!-- Twitter -->' . "\n" .
+                                '<meta name="twitter:card" content="summary" />' . "\n" .
+                                '<meta name="twitter:title" content="Plugin socialMeta 0.2 pour Dotclear" />' . "\n" .
+                                '<meta name="twitter:description" content="Nouvelle version de ce petit plugin, ..." />' . "\n" .
+                                '<meta name="twitter:image" content="http://open-time.net/public/illustrations/2014/.googleplus-twitter-facebook_m.jpg"/>' . "\n" .
+                                '<meta name="twitter:image:alt" content="G+, Twitter et Facebook"/>' . "\n" .
+                                '<meta name="twitter:site" content="@franckpaul" />' . "\n" .
+                                '<meta name="twitter:creator" content="@franckpaul" />' . "\n"
+                            )
+                        ),
+                    ]),
+                // Settings
+                formFieldset::init('sm_conf')
+                    ->legend(formLegend::init(__('Settings')))
+                    ->class('fieldset')
+                    ->fields([
+                        // Twitter account
+                        formPara::init()
+                            ->separator(' ')
+                            ->items([
+                                formLabel::init(__('Twitter account:'))
+                                    ->for('sm_twitter_account'),
+                                formInput::init('sm_twitter_account')
+                                    ->value(html::escapeHTML($sm_twitter_account))
+                                    ->size(30)
+                                    ->maxlength(128)
+                                    ->extra('aria-describedby="prefix-twitter_account"'),
+                            ]),
+                        formPara::init('prefix-twitter_account')
+                            ->class('form-note')
+                            ->items([
+                                formText::init('', __('With or without @ prefix.')),
+                            ]),
+                        // Photoblog
+                        formPara::init()
+                            ->separator(' ')
+                            ->items([
+                                formCheckbox::init('sm_photo', $sm_photo)
+                                    ->value(1),
+                                formLabel::init(__('This blog is a photoblog'))
+                                    ->for('sm_photo')
+                                    ->class('classic')
+                                    ->extra('aria-describedby="summary_large_image"'),
+                            ]),
+                        formPara::init('summary_large_image')
+                            ->class('form-note')
+                            ->items([
+                                formText::init('', __('Will use "summary_large_image" twitter card type rather than "summary", and will include the first original photo if possible rather than the medium thumbnail.')),
+                            ]),
+                        // Default description
+                        formPara::init()
+                            ->separator(' ')
+                            ->items([
+                                formLabel::init(__('Default description:'))
+                                    ->for('sm_description'),
+                                formInput::init('sm_description')
+                                    ->value(html::escapeHTML($sm_description))
+                                    ->size(80)
+                                    ->maxlength(255)
+                                    ->extra('aria-describedby="default_description"'),
+                            ]),
+                        formPara::init('default_description')
+                            ->class('form-note')
+                            ->items([
+                                formText::init('', __('Will be used if post (or page) have no text.')),
+                            ]),
+                        // Default image
+                        formPara::init()
+                            ->separator(' ')
+                            ->items([
+                                formLabel::init(__('Default image (URL):'))
+                                    ->for('sm_image'),
+                                formInput::init('sm_image')
+                                    ->value(html::escapeHTML($sm_image))
+                                    ->size(80)
+                                    ->maxlength(255)
+                                    ->extra('aria-describedby="default_image"'),
+                            ]),
+                        formPara::init('default_image')
+                            ->class('form-note')
+                            ->items([
+                                formText::init('', __('Will be used if post (or page) have no image.')),
+                            ]),
+                    ]),
+                // Button
+                formPara::init()
+                    ->items([
+                        formSubmit::init('frmsave')
+                            ->accesskey('s')
+                            ->value(__('Save')),
+                        dcCore::app()->formNonce(false),
+                    ]),
+            ])
+            ->render();
 
-        '<h3>' . __('Options') . '</h3>' .
-
-        '<p>' . form::checkbox('sm_on_post', 1, $sm_on_post) . ' ' .
-        '<label for="sm_on_post" class="classic">' . __('Add social meta on posts') . '</label></p>' .
-        '<p>' . form::checkbox('sm_on_page', 1, $sm_on_page) . ' ' .
-        '<label for="sm_on_page" class="classic">' . __('Add social meta on pages') . '</label></p>' .
-
-        '<hr />' .
-
-        '<p>' . form::checkbox('sm_facebook', 1, $sm_facebook) . ' ' .
-        '<label for="sm_facebook" class="classic">' . __('Use Facebook social meta:') . '</label></p>' .
-        '<pre>' .
-        html::escapeHTML(
-            '<!-- Facebook -->' . "\n" .
-            '<meta property="og:title" content="Plugin socialMeta 0.2 pour Dotclear" />' . "\n" .
-            '<meta property="og:url" content="http://open-time.net/post/2014/01/20/Plugin-socialMeta-02-pour-Dotclear" />' . "\n" .
-            '<meta property="og:site_name" content="Open-Time" />' . "\n" .
-            '<meta property="og:description" content="Nouvelle version de ce petit plugin, ..." />' . "\n" .
-            '<meta property="og:image" content="http://open-time.net/public/illustrations/2014/.googleplus-twitter-facebook_m.jpg" />' . "\n"
-        ) .
-        '</pre>' .
-
-        '<p>' . form::checkbox('sm_google', 1, $sm_google) . ' ' .
-        '<label for="sm_google" class="classic">' . __('Use Google social meta:') . '</label></p>' .
-        '<pre>' .
-        html::escapeHTML(
-            '<!-- Google -->' . "\n" .
-            '<meta itemprop="name" content="Plugin socialMeta 0.2 pour Dotclear" />' . "\n" .
-            '<meta itemprop="description" content="Nouvelle version de ce petit plugin, ..." />' . "\n" .
-            '<meta itemprop="image" content="http://open-time.net/public/illustrations/2014/.googleplus-twitter-facebook_m.jpg" />' . "\n"
-        ) .
-        '</pre>' .
-
-        '<p>' . form::checkbox('sm_twitter', 1, $sm_twitter) . ' ' .
-        '<label for="sm_twitter" class="classic">' . __('Use Twitter social meta:') . '</label></p>' .
-        '<pre>' .
-        html::escapeHTML(
-            '<!-- Twitter -->' . "\n" .
-            '<meta name="twitter:card" content="summary" />' . "\n" .
-            '<meta name="twitter:title" content="Plugin socialMeta 0.2 pour Dotclear" />' . "\n" .
-            '<meta name="twitter:description" content="Nouvelle version de ce petit plugin, ..." />' . "\n" .
-            '<meta name="twitter:image" content="http://open-time.net/public/illustrations/2014/.googleplus-twitter-facebook_m.jpg"/>' . "\n" .
-            '<meta name="twitter:image:alt" content="G+, Twitter et Facebook"/>' . "\n" .
-            '<meta name="twitter:site" content="@franckpaul" />' . "\n" .
-            '<meta name="twitter:creator" content="@franckpaul" />' . "\n"
-        ) .
-        '</pre>' .
-
-        '<h3>' . __('Settings') . '</h3>' .
-
-        '<p><label for="sm_twitter_account">' . __('Twitter account:') . '</label> ' .
-        form::field('sm_twitter_account', 30, 128, html::escapeHTML($sm_twitter_account), '', '', false, 'aria-describedby="prefix-twitter_account"') . '</p>' .
-        '<p class="form-note" id="prefix-twitter_account">' . __('With or without @ prefix.') . '</p>' .
-
-        '<p>' . form::checkbox('sm_photo', 1, $sm_photo, '', '', false, 'aria-describedby="summary_large_image"') . ' ' .
-        '<label for="sm_photo" class="classic">' . __('This blog is a photoblog') . '</label></p>' .
-        '<p class="form-note" id="summary_large_image">' . __('Will use "summary_large_image" twitter card type rather than "summary", and will include the first original photo if possible rather than the medium thumbnail.') . '</p>' .
-
-        '<p><label for="sm_description">' . __('Default description:') . '</label> ' .
-        form::field('sm_description', 80, 255, html::escapeHTML($sm_description), '', '', false, 'aria-describedby="default_description"') . '</p>' .
-        '<p class="form-note" id="default_description">' . __('Will be used if post (or page) have no text.') . '</p>' .
-
-        '<p><label for="sm_image">' . __('Default image (URL):') . '</label> ' .
-        form::field('sm_image', 80, 255, html::escapeHTML($sm_image), '', '', false, 'aria-describedby="default_image"') . '</p>' .
-        '<p class="form-note" id="default_image">' . __('Will be used if post (or page) have no image.') . '</p>' .
-
-        '<p>' . dcCore::app()->formNonce() . '<input type="submit" value="' . __('Save') . '" /></p>' .
-        '</form>';
-
-        echo
-        '</body>' .
-        '</html>';
+        self::closeModule();
     }
 }
