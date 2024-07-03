a40765513a2fb72482e8812a4000a25300157583
franck <carnet.franck.paul@gmail.com>
2024-07-03 09:29:53 +0200

    Remove first @ in mastodon @name@domain account (see https://github.com/mastodon/mastodon/pull/30398)

--- src/FrontendBehaviors.php
+++ src/FrontendBehaviors.php
@@ -99,13 +99,14 @@ class FrontendBehaviors
                 if ($settings->facebook) {
                     // Mastodon account
                     $account = (string) $settings->mastodon_account;
-                    if (strlen($account) && !str_starts_with($account, '@')) {
-                        $account = '@' . $account;
+                    if (strlen($account) && str_starts_with($account, '@')) {
+                        // Remove the first @ in @name@domain (see https://github.com/mastodon/mastodon/pull/30398)
+                        $account = ltrim($account, '@');
                     }
 
                     // Facebook/Mastodon meta
                     echo
-                    '<!-- Facebook -->' . "\n" .
+                    '<!-- Facebook/Mastodon (Open Graph) -->' . "\n" .
                     '<meta property="og:type" content="article">' . "\n" .
                     '<meta property="og:title" content="' . $title . '">' . "\n" .
                     '<meta property="og:url" content="' . $url . '">' . "\n" .
